99d55bda2544897c7021446f4318221974c6281e
yangdepu <yangdepp@163.com>
2020-03-30 13:03:06 +0800

    footer

--- blog-client/src/page/components/layout/index.jsx
+++ blog-client/src/page/components/layout/index.jsx
@@ -23,7 +23,17 @@ class CommonLayout extends React.Component {
           </Content>
           <Footer className="footer">
             <div className="content-1200">
-              <div className="footer-content" />
+              <div className="footer-content">
+                <div style={{ textAlign: 'center' }}>
+                  黄淮学院 版权所有 豫ICP备11031969号 通讯地址：河南省驻马店市开源大道76号
+                  邮政编码：463000
+                </div>
+                <div style={{ textAlign: 'center', marginTop:'10px' }}>
+                  校办：+86-396-2853503 传真：+86-396-2853115 招生：+86-396-2853111
+                  网络安全举报：+86-396-2853351 nic@huanghuai.edu.cn
+                </div>
+                <div style={{ textAlign: 'center', marginTop: '20px' }}>豫公网安备 41170202000114号</div>
+              </div>
             </div>
           </Footer>
         </Layout>
